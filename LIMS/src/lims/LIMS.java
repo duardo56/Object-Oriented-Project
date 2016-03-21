5a7325c159bb2b84392b4c964edfde2e4a154671
deltronzero94 <kmzero94@gmail.com>
2016-03-21 12:30:23 -0500

    fsdfsdfsd
    fsd

---
+++ LIMS/src/lims/LIMS.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lims;
+
+/**
+ *
+ * @author reticent
+ */
+public class LIMS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        MainMenu gui = new MainMenu();
+        gui.setVisible(true);
+    }
+    
+}
