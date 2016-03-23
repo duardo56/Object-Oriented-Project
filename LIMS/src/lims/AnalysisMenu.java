9490faf5b7e1236c33aafe365338325a8ec7ca57
deltronzero94 <kmzero94@gmail.com>
2016-03-22 19:32:50 -0500

    JList Working
    JList is now working, just need to find a way to fill text files as you navigate the JList

--- LIMS/src/lims/AnalysisMenu.java
+++ LIMS/src/lims/AnalysisMenu.java
@@ -5,19 +5,27 @@
  */
 package lims;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import javax.swing.JTabbedPane;
+import java.util.Scanner;
+import javax.swing.DefaultListModel;
+import javax.swing.JList;
 
 /**
  *
  * @author reticent
  */
-public class AnalysisMenu extends javax.swing.JFrame {
+public class AnalysisMenu extends javax.swing.JFrame  {
 
     /**
      * Creates new form AnalysisMenu
      */
     public AnalysisMenu() {
         initComponents();
+        
+        //fills the sample list
+        fillSampleList();
     }
 
     /**
@@ -36,13 +44,15 @@ public class AnalysisMenu extends javax.swing.JFrame {
         jLabel2 = new javax.swing.JLabel();
         jLabel3 = new javax.swing.JLabel();
         jLabel4 = new javax.swing.JLabel();
-        lblCompany = new javax.swing.JLabel();
+        lblType = new javax.swing.JLabel();
         jScrollPane1 = new javax.swing.JScrollPane();
-        jList1 = new javax.swing.JList<>();
+        listSampleList = new javax.swing.JList<>();
         jLabel6 = new javax.swing.JLabel();
-        lblCompany1 = new javax.swing.JLabel();
-        lblCompany2 = new javax.swing.JLabel();
-        lblCompany3 = new javax.swing.JLabel();
+        lblCompany = new javax.swing.JLabel();
+        lblStatus = new javax.swing.JLabel();
+        lblID = new javax.swing.JLabel();
+        jButton1 = new javax.swing.JButton();
+        jButton2 = new javax.swing.JButton();
         jMenuBar1 = new javax.swing.JMenuBar();
         jMenu3 = new javax.swing.JMenu();
         menuItemLogOut = new javax.swing.JMenuItem();
@@ -91,41 +101,69 @@ public class AnalysisMenu extends javax.swing.JFrame {
 
         jLabel4.setText("ID:");
 
-        lblCompany.setBorder(new javax.swing.border.SoftBevelBorder(javax.swing.border.BevelBorder.LOWERED));
-        lblCompany.setPreferredSize(new java.awt.Dimension(10, 30));
+        lblType.setBorder(new javax.swing.border.SoftBevelBorder(javax.swing.border.BevelBorder.LOWERED));
+        lblType.setPreferredSize(new java.awt.Dimension(10, 30));
 
-        jScrollPane1.setViewportView(jList1);
+        listSampleList.addListSelectionListener(new javax.swing.event.ListSelectionListener() {
+            public void valueChanged(javax.swing.event.ListSelectionEvent evt) {
+                listSampleListValueChanged(evt);
+            }
+        });
+        jScrollPane1.setViewportView(listSampleList);
 
         jLabel6.setText("Current List of Samples");
 
-        lblCompany1.setBorder(new javax.swing.border.SoftBevelBorder(javax.swing.border.BevelBorder.LOWERED));
-        lblCompany1.setPreferredSize(new java.awt.Dimension(10, 30));
+        lblCompany.setBorder(new javax.swing.border.SoftBevelBorder(javax.swing.border.BevelBorder.LOWERED));
+        lblCompany.setPreferredSize(new java.awt.Dimension(10, 30));
 
-        lblCompany2.setBorder(new javax.swing.border.SoftBevelBorder(javax.swing.border.BevelBorder.LOWERED));
-        lblCompany2.setPreferredSize(new java.awt.Dimension(10, 30));
+        lblStatus.setBorder(new javax.swing.border.SoftBevelBorder(javax.swing.border.BevelBorder.LOWERED));
+        lblStatus.setPreferredSize(new java.awt.Dimension(10, 30));
 
-        lblCompany3.setBorder(new javax.swing.border.SoftBevelBorder(javax.swing.border.BevelBorder.LOWERED));
-        lblCompany3.setPreferredSize(new java.awt.Dimension(10, 30));
+        lblID.setBorder(new javax.swing.border.SoftBevelBorder(javax.swing.border.BevelBorder.LOWERED));
+        lblID.setPreferredSize(new java.awt.Dimension(10, 30));
+
+        jButton1.setText("Refresh");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+
+        jButton2.setText("Submit");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout paneViewSampleLayout = new javax.swing.GroupLayout(paneViewSample);
         paneViewSample.setLayout(paneViewSampleLayout);
         paneViewSampleLayout.setHorizontalGroup(
             paneViewSampleLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(paneViewSampleLayout.createSequentialGroup()
-                .addGroup(paneViewSampleLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                    .addComponent(jLabel6, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE))
-                .addGap(24, 24, 24)
                 .addGroup(paneViewSampleLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jLabel3, javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addComponent(jLabel4, javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addComponent(jLabel2, javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addComponent(jLabel1, javax.swing.GroupLayout.Alignment.TRAILING))
+                    .addGroup(paneViewSampleLayout.createSequentialGroup()
+                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 161, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGroup(paneViewSampleLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, paneViewSampleLayout.createSequentialGroup()
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                .addComponent(jLabel3))
+                            .addGroup(paneViewSampleLayout.createSequentialGroup()
+                                .addGap(18, 18, 18)
+                                .addGroup(paneViewSampleLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 69, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                    .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 69, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                    .addGroup(paneViewSampleLayout.createSequentialGroup()
+                                        .addGap(18, 18, 18)
+                                        .addComponent(jLabel1))
+                                    .addComponent(jLabel4, javax.swing.GroupLayout.Alignment.TRAILING)))))
+                    .addComponent(jLabel6)
+                    .addComponent(jLabel2, javax.swing.GroupLayout.Alignment.TRAILING))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 27, Short.MAX_VALUE)
                 .addGroup(paneViewSampleLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                    .addComponent(lblCompany1, javax.swing.GroupLayout.DEFAULT_SIZE, 287, Short.MAX_VALUE)
-                    .addComponent(lblCompany3, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(lblCompany2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(lblType, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 275, Short.MAX_VALUE)
+                    .addComponent(lblID, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(lblStatus, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                     .addComponent(lblCompany, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                 .addGap(34, 34, 34))
         );
@@ -140,26 +178,28 @@ public class AnalysisMenu extends javax.swing.JFrame {
                         .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 205, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addGap(0, 0, Short.MAX_VALUE))
                     .addGroup(paneViewSampleLayout.createSequentialGroup()
-                        .addGroup(paneViewSampleLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
-                            .addGroup(paneViewSampleLayout.createSequentialGroup()
-                                .addComponent(jLabel1)
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                                .addComponent(jLabel2))
-                            .addGroup(paneViewSampleLayout.createSequentialGroup()
-                                .addComponent(lblCompany1, javax.swing.GroupLayout.PREFERRED_SIZE, 17, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                .addComponent(lblCompany, javax.swing.GroupLayout.PREFERRED_SIZE, 17, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                        .addGroup(paneViewSampleLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addComponent(lblCompany, javax.swing.GroupLayout.PREFERRED_SIZE, 17, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jLabel1))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addGroup(paneViewSampleLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addComponent(jLabel2)
+                            .addComponent(lblType, javax.swing.GroupLayout.PREFERRED_SIZE, 17, javax.swing.GroupLayout.PREFERRED_SIZE))
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addGroup(paneViewSampleLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addComponent(jLabel3)
-                            .addComponent(lblCompany2, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 17, javax.swing.GroupLayout.PREFERRED_SIZE))
+                            .addComponent(lblStatus, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 17, javax.swing.GroupLayout.PREFERRED_SIZE))
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addGroup(paneViewSampleLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addGroup(paneViewSampleLayout.createSequentialGroup()
-                                .addComponent(lblCompany3, javax.swing.GroupLayout.PREFERRED_SIZE, 17, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addComponent(lblID, javax.swing.GroupLayout.PREFERRED_SIZE, 17, javax.swing.GroupLayout.PREFERRED_SIZE)
                                 .addContainerGap(199, Short.MAX_VALUE))
                             .addGroup(paneViewSampleLayout.createSequentialGroup()
                                 .addComponent(jLabel4)
+                                .addGap(51, 51, 51)
+                                .addComponent(jButton1)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                                .addComponent(jButton2)
                                 .addGap(0, 0, Short.MAX_VALUE))))))
         );
 
@@ -216,29 +256,84 @@ public class AnalysisMenu extends javax.swing.JFrame {
         window.setVisible(true);    
     }//GEN-LAST:event_menuItemLogOutActionPerformed
 
+    //Closes the program when exit from Menu is clicked
     private void jMenuItem2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem2ActionPerformed
         System.exit(0); //closes the program
     }//GEN-LAST:event_jMenuItem2ActionPerformed
 
+   
     private void jTabbedPane1MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jTabbedPane1MouseClicked
        JTabbedPane tabbedPane= (JTabbedPane)evt.getSource();
        int tab = tabbedPane.getSelectedIndex();
        
        //Implement some way to let the program know what current tab its on
        //and to not allow it to run if it is already on the current tab.
-       
-       //If Analysis Tab is clicked on 
-       if (tab == 0){
-           javax.swing.JOptionPane.showMessageDialog(null, "Analysis Tab was clicked!");
-       } 
-       
-       //If View Sample tab is clicked on
-       else if (tab == 1){
-            javax.swing.JOptionPane.showMessageDialog(null, "View Sample Tab was clicked!");
-       }    
-        
+       if (tabNum ==-1){
+           
+           //If Analysis Tab is clicked on for the 1st time
+           if (tab == 0 ){
+               tabNum = tab;
+               javax.swing.JOptionPane.showMessageDialog(null, "Analysis Tab was clicked!");
+           } 
+
+           //If View Sample tab is clicked on for the 1st time
+           else if (tab == 1){
+               tabNum = tab;
+               javax.swing.JOptionPane.showMessageDialog(null, "View Sample Tab was clicked!");
+           }    
+        }
     }//GEN-LAST:event_jTabbedPane1MouseClicked
 
+    //Submits Sample Analysis to manager/management for further approval
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        /*
+        Code dedicated to submit changes to the sample analysis to the management
+        for further approval before client ends up receiving the final analysis.
+        Client may be able to see updates, but may not see final analysis until 
+        approved by management.
+        */
+    }//GEN-LAST:event_jButton2ActionPerformed
+
+    //Refreshed current Sample List available to analysis
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+       /*
+        
+        */
+    }//GEN-LAST:event_jButton1ActionPerformed
+
+    //When the User changes between files in the list
+    private void listSampleListValueChanged(javax.swing.event.ListSelectionEvent evt) {//GEN-FIRST:event_listSampleListValueChanged
+        JList test = (JList)evt.getSource();
+        int num =test.getSelectedIndex();
+        javax.swing.JOptionPane.showMessageDialog(null,num);
+    }//GEN-LAST:event_listSampleListValueChanged
+    
+    //Method to fill the Jlist with samples that analysis is working on
+    private void fillSampleList(){
+        listModel = new DefaultListModel();
+        
+        //Can implement an algorithm where it searchers files that
+        //the analysis is currently working on then adds those files
+        //to the list.
+        File loginf = new File("userSample1");
+        
+        try{
+            Scanner read = new Scanner(loginf);  
+        }
+        catch(FileNotFoundException q){
+            javax.swing.JOptionPane.showMessageDialog(null,"Can't find a text file");
+        }
+        listModel.addElement(loginf.getName()); //gets the name of the file
+        listModel.addElement("Testing Number 1");   //adds another element for testing purpose
+        
+        listSampleList.setModel(listModel); //adds elements to the JList
+    }
+    
+    //Reads from file the Client's submitted information
+    private void readClientFile(){
+        
+    }
+    
     /**
      * @param args the command line arguments
      */
@@ -273,14 +368,19 @@ public class AnalysisMenu extends javax.swing.JFrame {
             }
         });
     }
-
+    
+    //Declared Variables
+    private DefaultListModel listModel;  //keeps track of files assign to analysis
+    private int tabNum = -1; //Keeps track of the current tab user is on
+    
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
     private javax.swing.JLabel jLabel6;
-    private javax.swing.JList<String> jList1;
     private javax.swing.JMenu jMenu3;
     private javax.swing.JMenuBar jMenuBar1;
     private javax.swing.JMenuItem jMenuItem2;
@@ -288,9 +388,10 @@ public class AnalysisMenu extends javax.swing.JFrame {
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTabbedPane jTabbedPane1;
     private javax.swing.JLabel lblCompany;
-    private javax.swing.JLabel lblCompany1;
-    private javax.swing.JLabel lblCompany2;
-    private javax.swing.JLabel lblCompany3;
+    private javax.swing.JLabel lblID;
+    private javax.swing.JLabel lblStatus;
+    private javax.swing.JLabel lblType;
+    private javax.swing.JList<String> listSampleList;
     private javax.swing.JMenuItem menuItemLogOut;
     private javax.swing.JPanel paneViewSample;
     // End of variables declaration//GEN-END:variables
