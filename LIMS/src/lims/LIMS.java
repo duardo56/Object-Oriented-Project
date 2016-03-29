176b26910bde7f58a307a20c13345994d5fe6e3c
deltronzero94 <kmzero94@gmail.com>
2016-03-29 13:28:32 -0500

    Minor Clean up in files

--- LIMS/src/lims/LIMS.java
+++ LIMS/src/lims/LIMS.java
@@ -6,8 +6,12 @@
 package lims;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutput;
 import java.io.ObjectOutputStream;
 import java.io.OutputStream;
@@ -24,17 +28,34 @@ public class LIMS {
         /*
             Testing: User class and System Class
         */
-        
-        User user = new User();
-        user.setPassword("Password");
-        user.setUsername("User");
-        
-        File file = new File("testFile");
-        OutputStream fileOutputStream = new FileOutputStream(file);
-        ObjectOutput outputStream = new ObjectOutputStream(fileOutputStream);
-        outputStream.writeObject(user);
-        
-        System.out.println("I've stored the User object into the file: " + file.getName());
+        try{
+            User user = new User();
+            user.setPassword("Password");
+            user.setUsername("User");
+
+            File file = new File("testFile");
+            OutputStream fileOutputStream = new FileOutputStream(file);
+            ObjectOutput outputStream = new ObjectOutputStream(fileOutputStream);
+            outputStream.writeObject(user);
+
+            System.out.println("I've stored the User object into the file: " + file.getName());
+
+            //Input Stream
+            FileInputStream fInput = new FileInputStream("testFile");
+            ObjectInputStream ois = new ObjectInputStream(fInput);
+            User result = (User)ois.readObject();
+            
+            System.out.println("Username: "+result.getUsername() + "Password: "+result.getPassword());
+        }
+        catch (FileNotFoundException e){
+            e.printStackTrace();
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
+        catch(ClassNotFoundException e){
+            e.printStackTrace();
+        }
         
         //***********************************************************
         //LoginMenu gui = new LoginMenu();    //Creates LoginMenu Gui
